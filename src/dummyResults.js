55f81a861ee2e0fa477489c335549533718d97e6
causey96bc <causey904.bc@gmail.com>
2021-05-14 13:28:21 -0400

    Adding dummy data for reference

---
+++ src/dummyResults.js
@@ -0,0 +1,47 @@
+export default [
+    {
+      agency_code: "021048",
+      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
+      ivans_account: "YD6X2",
+    },
+    {
+      agency_code: "021048005",
+      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
+      ivans_account: "YD6X2",
+    },
+    {
+      agency_code: "021048010",
+      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
+      ivans_account: "        ",
+    },
+    {
+      agency_code: "021048013",
+      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
+      ivans_account: "YD6X2",
+    },
+    {
+      agency_code: "100338",
+      agency_name: "PCF INSURANCE SVS OF THE WEST ",
+      ivans_account: "YD6X2",
+    },
+    {
+      agency_code: "100338002",
+      agency_name: "PCF INSURANCE SVS OF THE WEST ",
+      ivans_account: "YD6X2",
+    },
+    {
+      agency_code: "100338003",
+      agency_name: "PCF INSURANCE SVS OF THE WEST ",
+      ivans_account: "YD6X2",
+    },
+    {
+      agency_code: "100338005",
+      agency_name: "PCF INSURANCE SVS OF THE WEST ",
+      ivans_account: "YD6X2",
+    },
+    {
+      agency_code: "100338013",
+      agency_name: "PCF INSURANCE SVS OF THE WEST ",
+      ivans_account: "YD6X2",
+    },
+  ];
