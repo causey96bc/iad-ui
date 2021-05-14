d67f43c867deb7135fd1cb3381b5eb2b9faaaeeb
causey96bc <causey904.bc@gmail.com>
2021-05-14 13:30:42 -0400

    adding activeRow state for dynamic rendering of cb

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,96 +3,30 @@ import "./SearchBar.css";
 import  User  from "./components/User";
 import { searchKeys, lines, claimsSel, secondary } from "./metaconfig";
 import CheckBoxes from "./components/Checkboxes";
+
 const SearchPage = (event, user) => {
   const [filter, setFilter] = useState("agency_name");
   const [searchStr, setSearchStr] = useState("");
+  const [activeRow, setActiveRow] = useState(null);
   const [matches, setMatches] = useState([]);
   let indicators = {};
-  let searchString = "";
-  let results = [
-    {
-      agency_code: "021048",
-      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
-      ivans_account: "YD6X2",
-    },
-    {
-      agency_code: "021048005",
-      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
-      ivans_account: "YD6X2",
-    },
-    {
-      agency_code: "021048010",
-      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
-      ivans_account: "        ",
-    },
-    {
-      agency_code: "021048013",
-      agency_name: "PCF INS SVS DBA CAPITAL WEST  ",
-      ivans_account: "YD6X2",
-    },
-    {
-      agency_code: "100338",
-      agency_name: "PCF INSURANCE SVS OF THE WEST ",
-      ivans_account: "YD6X2",
-    },
-    {
-      agency_code: "100338002",
-      agency_name: "PCF INSURANCE SVS OF THE WEST ",
-      ivans_account: "YD6X2",
-    },
-    {
-      agency_code: "100338003",
-      agency_name: "PCF INSURANCE SVS OF THE WEST ",
-      ivans_account: "YD6X2",
-    },
-    {
-      agency_code: "100338005",
-      agency_name: "PCF INSURANCE SVS OF THE WEST ",
-      ivans_account: "YD6X2",
-    },
-    {
-      agency_code: "100338013",
-      agency_name: "PCF INSURANCE SVS OF THE WEST ",
-      ivans_account: "YD6X2",
-    },
-  ];
-  const debounce =  (func, timeout = 500) => {
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
-    };
-  }
-  const fetchData = async () => {
-    const url = "https://q5gjy1glol.execute-api.us-east-1.amazonaws.com/dev/hello-2"
-    const data =  await fetch(url);
-    const info = await data.json();
-    console.log("this is the data", info);
-  }
   const searchData = async (event) => {
-    let input = event.target.value;
-    let searchStr = input;
-    setSearchStr(searchStr);
+    event.preventDefault()
     const url = `https://q5gjy1glol.execute-api.us-east-1.amazonaws.com/dev/search/${searchStr}`;
     const results = await fetch(url);
     const data = await results.json();
-    console.log("data", data.items);
-    // setMatches([))
+    if(data && data.items && data.items[0]){
+      console.log("data", data);
+      // for (const [key, value] of Object.keys(data.items[0])){
+      //   console.log(`${key}: ${value}`);
+      // } 
+      setMatches(data.items);
+      setActiveRow(data.items[0]);
+    } else {
+      setMatches([]);
+      setActiveRow(null);
+    }
   }
-  const search = (event) => {
-    event.preventDefault();
-    // let finding = results.find(result => (result[filter] === searchStr))
-    const matches = results.filter((result) => {
-      result.checked = false;
-      return result[filter].includes(searchStr);
-    });
-    console.log(matches, "matches");
-    setMatches(matches);
-  };
-
-  // const searchHandler = (event, searchStr) => {
-    
-  // };
   const updateInds = (event) => {
     if (event.target.type === "checkbox") {
       indicators[event.target.name] =
@@ -102,151 +36,140 @@ const SearchPage = (event, user) => {
   };
   const save = (e) => {
     e.preventDefault()  
-    // indic
     // indicators = {}
-    // result = []
+    // form reset
     // console.log("you saved your selections", indicators);
   }
 
   return (
     <main className="container searchBar">
       <div className="d-flex">
-        <form className="form-group" onSubmit={search}>
-        <div class="row results">
-         <div class="locations col row">
-          <div className="input-group">
-            <div className="input-group-text">Search By</div>
-            <select
-              className="input-group-text"
-              onChange={(event) => {
-                setFilter(event.target.value);
-              }}
-            >
-              {searchKeys.map((searchKey, key) => {
-                const [machine_name, human_name] = searchKey;
-                return (
-                  <option value={machine_name} key={key}>
-                    {human_name}
-                  </option>
-                );
-              })}
-            </select>
-            <input
-              className="form-control"
-              onChange={searchData}
-              placeholder="type part of the search key and press ENTER"
-            />
-            <button className="btn btn-primary" type="submit">
-              Search
-            </button>
-          </div>
-          </div>
+        <form className="form-group" onSubmit={searchData}>
+          <div class="row results">
+            <div class="locations col row">
+              <div className="input-group">
+                <div className="input-group-text">Search By</div>
+                <select
+                  className="input-group-text"
+                  onChange={(event) => {
+                    setFilter(event.target.value);
+                  }}
+                >
+                  {searchKeys.map((searchKey, key) => {
+                    const [machine_name, human_name] = searchKey;
+                    return (
+                      <option value={machine_name} key={key}>
+                        {human_name}
+                      </option>
+                    );
+                  })}
+                </select>
+                <input
+                  className="form-control"
+                  onChange={e => setSearchStr(e.target.value)}
+                  placeholder="type part of the search key and press ENTER"
+                />
+                <button className="btn btn-primary" type="submit">
+                  Search
+                </button>
+              </div>
+            </div>
           </div>
         </form>
       </div>
       <form onSubmit={save}>
-      <div class="row results">
-        <div class="locations col row">
-          <span>
-            <h4>Agencies</h4>
-          </span>
-          <table class="table table-sm table-hover table-borderless">
-            <thead>
-              <th>
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  id="locations-sel-all"
-                  title="select all"
-                />
-              </th>
-              <th>
-                <label for="locations-sel-all" class="form-check-label">
-                  code
-                </label>
-              </th>
-              <th>
-                <label for="locations-sel-all" class="form-check-label">
-                  account
-                </label>
-              </th>
-              <th>
-                <label for="locations-sel-all" class="form-check-label">
-                  name
-                </label>
-              </th>
-            </thead>
-            <tbody>
-              {/* {#each result.result as [code, name, account], index (index)} */}
-              {matches.map((result) => {
-                return (
-                  <tr>
+        <div class="row results">
+          <div class="locations col row">
+            <span>
+              <h4>Agencies</h4>
+            </span>
+            <table class="table table-sm table-hover table-borderless">
+              <thead>
+                <th>
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="locations-sel-all"
+                    title="select all"
+                  />
+                </th>
+                <th>
+                  <label for="locations-sel-all" class="form-check-label">
+                    code
+                  </label>
+                </th>
+                <th>
+                  <label for="locations-sel-all" class="form-check-label">
+                    account
+                  </label>
+                </th>
+                <th>
+                  <label for="locations-sel-all" class="form-check-label">
+                    name
+                  </label>
+                </th>
+              </thead>
+              <tbody>
+                {/* {#each result.result as [code, name, account], index (index)} */}
+                {matches.map((row) => (
+                  <tr onClick={() => setActiveRow(row)}>
                     <td>
                       <input type="checkbox" class="form-check-input" />
                     </td>
-
-                    {Object.keys(result).map((item) => {
-                      return <td>{result[item]}</td>;
-                    })}
+                    <td>{row.agency_code}</td>
+                    <td>{row.ivans_account}</td>
+                    <td>{row.agency_name}</td>
                   </tr>
-                );
-              })}
-              <tr>
-                <td>
-                  <input type="checkbox" class="form-check-input" />
-                </td>
-                <td>
-                  <label class="form-check-label"></label>
-                </td>
-                <td>
-                  <label class="form-check-label"></label>
-                </td>
-                <td>
-                  <label class="form-check-label"></label>
-                </td>
-              </tr>
-              <tr>
-                <td colspan="4">Search for something.</td>
-              </tr>
-            </tbody>
-          </table>
+                ))}
+              </tbody>
+            </table>
           </div>
           <div class="col details">
-          <h4>Agency Details</h4>
-          <div class="details">Some details ... from IVANS/PMACS/AIDB</div>
+            <h4>Agency Details</h4>
+            <div class="details"><p>Some details ... from IVANS/PMACS/AIDB</p>
+            {activeRow && 
+              <ul>
+                <li>Last Update: {activeRow.last_upd_date}</li>
+                <li>Machine Address: {activeRow.ibm_machine_address}</li>
+              </ul>
+            }
+            </div>
           </div>
+          {activeRow &&
+          <>
           <div className="row selections">
-              <div className="services col">
+            <div className="services col">
               {lines.map((claims) => (
                 <CheckBoxes
                   data={claims}
-                  indicators={indicators}
+                  indicators={activeRow.dl_selections}
                   updateIndicators={(event) => updateInds(event)}
                 />
               ))}
-            <div className="secondary col">
-            {claimsSel.map((claims) => (
-            <CheckBoxes
-              data={claims}
-              indicators={indicators}
-              updateIndicators={(event) => updateInds(event)}
-            />
-          ))}
+              <div className="secondary col">
+                {claimsSel.map((claims) => (
+                  <CheckBoxes
+                    data={claims}
+                    indicators={activeRow.dl_selections}
+                    updateIndicators={(event) => updateInds(event)}
+                  />
+                ))}
+              </div>
+
+              {secondary.map((claims) => (
+                <CheckBoxes
+                  data={claims}
+                  indicators={activeRow.dl_selections}
+                  updateIndicators={(event) => updateInds(event)}
+                  type="radio"
+                />
+              ))}
+            </div>
           </div>
-          
-          {secondary.map((claims) => (
-            <CheckBoxes
-              data={claims}
-              indicators={indicators}
-              updateIndicators={(event) => updateInds(event)}
-              type="radio"
-            />
-          ))}
-          </div>
+          <button onClick={save}>Save!</button>
+          </>
+          }
         </div>
-      
-      <button onClick={save}>Save!</button>
-      </div>
       </form>
     </main>
   );
