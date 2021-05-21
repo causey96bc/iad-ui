fe134f6df598c74ba17103a2894a0c25cee7b3ec
causey96bc <causey904.bc@gmail.com>
2021-05-21 18:06:11 -0400

    add mobx to manage state across components

---
+++ src/SearchStore.js
@@ -0,0 +1,42 @@
+import { observable, action, makeObservable, computed, autorun } from "mobx";
+
+class SearchStore {
+  matches = [];
+  active = {};
+  constructor() {
+    makeObservable(this, {
+      matches: observable,
+      active: observable,
+      setMatches: action,
+      setActive: action,
+      getMatches: action,
+      getActive: action,
+      updateIndicators: action,
+      hasActive: computed,
+    });
+    autorun(this.logStoreDetails);
+  }
+
+  logStoreDetails = () => {
+    console.log("store logger");
+  };
+  setActive(active) {
+    this.active = active;
+  }
+  setMatches(matches) {
+    this.matches = matches;
+  }
+  updateIndicators({name, value}) {
+    const newActive = {};
+    // Update the dl_selectors object for the given indicator
+    Object.keys(this.active.dl_selections).map((indicator) => {
+      newActive[indicator] = indicator === name ? value : this.active.dl_selections.hasOwnProperty(indicator) ? this.active.dl_selections[indicator] : false;
+    });
+    this.active.dl_selections = newActive;
+  }
+  get hasActive() {
+    return Object.keys(this.active).length > 0 ? true : false
+  }
+}
+
+export default SearchStore
