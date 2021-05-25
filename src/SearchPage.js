b1783a10e2467cb6a7cb6513fe10925980ecbfda
causey96bc <causey904.bc@gmail.com>
2021-05-25 13:59:51 -0400

    add conditional rendering for spinner

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,90 +1,95 @@
 import React, { useState } from "react";
 import "./SearchPage.css";
-import {lines, claimsSel, secondary } from "./metaconfig";
+import { lines, claimsSel, secondary } from "./metaconfig";
 import { observer } from "mobx-react-lite";
 import { toJS } from 'mobx'
 import CheckBoxes from "./components/Checkboxes";
 import Search from "./components/Search";
 import SearchResults from "./components/SearchResults.js";
+import Messages from "./components/Messages";
+import { Spinner } from "react-bootstrap";
 
 
-const SearchPage = ({config, store}) => {
+const SearchPage = ({ config, store, messageStore }) => {
   async function updateAgency(row) {
     const url = `${config.api_url}/agency-location?id=${store.active.agency_code}`;
     const response = await fetch(url, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
+        "Authorization": "Bearer 1234"
       },
       body: JSON.stringify(store.active),
     });
     const json = await response.json();
     return json;
   }
-  async function save(e){
+  async function save(e) {
     e.preventDefault()
-    if(store.active){
+    if (store.active) {
       await updateAgency(store.active)
     }
-    else{
+    else {
       store.active = null
     }
-
   }
-
   return (
     <main className="container searchBar">
       <div className="d-flex">
-        <Search config={config} store={store}/>
+        <Search messageStore={messageStore} config={config} store={store} />
       </div>
       <form onSubmit={save}>
-        {store.matches.length > 0 ? 
-        <div>
-         <SearchResults store={store} config={config}/> 
-        </div>
-        :<></>}
+        {store.searching ?
+         <Spinner animation="border" role="status"></Spinner> 
+         : store.matches.length > 0 ?
+          <div>
+            <Messages messageStore={messageStore} />
+            <SearchResults store={store} config={config} />
+          </div>
+          : <>
+          </>}
         <div>
           {store.hasActive &&
-          <>
-          <div className="row selections">
-            <div className="services col">
-              {lines.map((claims, key) => (
-                <CheckBoxes
-                  store={store}
-                  key={key}
-                  data={claims}
-                  indicators={toJS(store.active)["dl_selections"]}
-                  //updateIndicators={(event) => updateInds(event)}
-                />
-              ))}
-              <div className="secondary col">
-                {claimsSel.map((claims, key) => (
-                  <CheckBoxes
-                    store={store}
-                    key={key}
-                    data={claims}
-                    indicators={toJS(store.active)["dl_selections"]}
+            <>
+              <div className="row selections">
+                <div className="services col">
+                  {lines.map((claims, key) => (
+                    <CheckBoxes
+                      store={store}
+                      key={key}
+                      data={claims}
+                      indicators={toJS(store.active)["dl_selections"]}
                     //updateIndicators={(event) => updateInds(event)}
-                  />
-                ))}
-              </div>
+                    />
+                  ))}
+                  <div className="secondary col">
+                    {claimsSel.map((claims, key) => (
+                      <CheckBoxes
+                        store={store}
+                        key={key}
+                        data={claims}
+                        indicators={toJS(store.active)["dl_selections"]}
+                      //updateIndicators={(event) => updateInds(event)}
+                      />
+                    ))}
+                  </div>
 
-              {secondary.map((claims, key) => (
-                <CheckBoxes
-                store={store}
-                  key={key}
-                  data={claims}
-                  indicators={toJS(store.active)["dl_selections"]}
-                  //updateIndicators={(event) => updateInds(event)}
-                  type="radio" 
-                />
-              ))}
-            </div>
-          </div>
-          <button className="btn btn-primary" type="submit">
-                  Save
+                  {secondary.map((claims, key) => (
+                    <CheckBoxes
+                      store={store}
+                      key={key}
+                      data={claims}
+                      indicators={toJS(store.active)["dl_selections"]}
+                      //updateIndicators={(event) => updateInds(event)}
+                      type="radio"
+                    />
+                  ))}
+                </div>
+              </div>
+              <button className="btn btn-primary" type="submit">
+                Save
                 </button>
-          </>
+            </>
           }
         </div>
       </form>
